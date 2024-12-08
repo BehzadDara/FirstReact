9bb75443557356e3384aa3565b54b9c66f3de010
behzad dara <Behzad.dara.99@gmail.com>
2024-12-08 18:29:20 +0330

    ComplementColor

--- src/components/ColorSpectrumGenerator.tsx
+++ src/components/ColorSpectrumGenerator.tsx
@@ -93,12 +93,30 @@ const ColorSpectrumGenerator: React.FC = () => {
             const hexValue = `#${color.hex}`;
             const rgbValue = `rgb(${color.rgb.join(",")})`;
 
+            const getReadableTextColor = (bgRgb: number[]): string => {
+              const [r, g, b] = bgRgb.map((c) => c / 255);
+            
+              const corrected = (c: number) =>
+                c <= 0.03928 ? c / 12.92 : Math.pow((c + 0.055) / 1.055, 2.4);
+            
+              const luminance =
+                0.2126 * corrected(r) + 0.7152 * corrected(g) + 0.0722 * corrected(b);
+            
+              const whiteLuminance = 1;
+              const blackLuminance = 0;
+            
+              const contrastWithWhite = (whiteLuminance + 0.05) / (luminance + 0.05);
+              const contrastWithBlack = (luminance + 0.05) / (blackLuminance + 0.05);
+            
+              return contrastWithWhite > contrastWithBlack ? "white" : "black";
+            };
+
             return (
               <Card
                 key={index}
                 sx={{
                   backgroundColor: hexValue,
-                  color: index > 9 ? "white" : "black",
+                  color: getReadableTextColor(color.rgb),
                   padding: 1,
                   textAlign: "center",
                   cursor: "pointer",
